9822ed4d98e2fe42f6259a31d27c444fe5e4a95d
em <35030185+em108@users.noreply.github.com>
2021-08-07 16:16:28 +0300

    small fix

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DST="" #Enter your destination ID
-re='([-\w]{25,})'
+re='([-\w]{18,})'
 readarray -t content < links.txt
 for link in "${content[@]}"; do
 	id=$( echo "$link" | grep --perl-regexp $re --only-matching )
